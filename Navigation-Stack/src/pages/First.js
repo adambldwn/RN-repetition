a004610c4539602f382a6d571bb4e2339cfa16c1
efe <adambldwn110@gmail.com>
2020-12-01 16:06:40 +0300

    stack

---
+++ Navigation-Stack/src/pages/First.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {SafeAreaView, View, Text, Button} from 'react-native';
+
+const number = 15;
+
+const First = (props) => {
+    return(
+        <SafeAreaView>
+            <View>
+                <Text style={{fontSize: 40}}>First</Text>
+                <Text style={{fontSize: 40}}>{number}</Text>
+                
+                <Button
+                    title="Go!"
+                    onPress={()=> props.navigation.navigate('SecondPage', {
+                        myNumber: number
+                    } )}
+                />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default First;
